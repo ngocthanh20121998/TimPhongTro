c258e19ca8c29ae674d008d0e2c65084a5b45335
NguyenNgocThanh <ngocthanh20121998@gmail.com>
2020-11-22 13:35:01 +0700

    App tim phong tro

---
+++ app/src/main/java/com/thanh/timphongtro/DangKy.java
@@ -0,0 +1,90 @@
+package com.thanh.timphongtro;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+
+
+public class DangKy extends Fragment {
+    EditText edtEmailDangKy, edtPasswordDangKy;
+    Button btnDangKy;
+
+    FirebaseAuth mAuthentication;
+
+   // final Context context = getContext();
+
+
+    public DangKy() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dang_ky, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mAuthentication = FirebaseAuth.getInstance();
+        Button btnTroVe = view.findViewById(R.id.buttonTroVe);
+        btnDangKy = view.findViewById(R.id.buttonDangKy);
+        edtEmailDangKy = view.findViewById(R.id.editTextEmail);
+        edtPasswordDangKy = view.findViewById(R.id.editTextPassword);
+
+        final NavController navController = Navigation.findNavController(view);
+
+        btnDangKy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DangKyTK();
+            }
+        });
+
+        btnTroVe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_dangKy_to_dangNhap);
+            }
+        });
+    }
+
+    private void DangKyTK(){
+        String email = edtEmailDangKy.getText().toString();
+        String password = edtPasswordDangKy.getText().toString();
+        mAuthentication.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        Toast.makeText(getContext(), "Đăng ký thành công!!", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(getContext(), "Tài khoản đã tồn tại!!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+    }
+
+    }
---
+++ app/src/main/java/com/thanh/timphongtro/DangNhap.java
@@ -0,0 +1,92 @@
+package com.thanh.timphongtro;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class DangNhap extends Fragment {
+    Button btnDangNhap;
+    EditText edtTaiKhoan, edtMatKhau;
+    FirebaseAuth mAuthentication;
+
+
+    public DangNhap() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dang_nhap, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button btnTaoTk = view.findViewById(R.id.buttonTaoTaiKhoan);
+        mAuthentication = FirebaseAuth.getInstance();
+        btnDangNhap = view.findViewById(R.id.buttonDangNhap);
+        edtTaiKhoan = view.findViewById(R.id.editTextEmailDangNhap);
+        edtMatKhau = view.findViewById(R.id.editTextPasswordDangNhap);
+
+        btnDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DangNhapTK();
+            }
+        });
+
+        final NavController navController = Navigation.findNavController(view);
+        btnTaoTk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_dangNhap_to_dangKy);
+            }
+        });
+    }
+    public void DangNhapTK(){
+        String email = edtTaiKhoan.getText().toString();
+        String password = edtMatKhau.getText().toString();
+
+        mAuthentication.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getContext() , "Đăng nhập thành công!!", Toast.LENGTH_SHORT).show();
+                        } else {
+
+                            Toast.makeText(getContext() , "Tài khoản hoặc mật khẫu sai!!", Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+}
