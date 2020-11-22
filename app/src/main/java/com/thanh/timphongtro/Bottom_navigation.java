f77cd3b02fcf8d02de28faa1c1920bfe0813da0b
NguyenNgocThanh <ngocthanh20121998@gmail.com>
2020-11-22 16:38:28 +0700

    navigation

---
+++ app/src/main/java/com/thanh/timphongtro/Bottom_navigation.java
@@ -0,0 +1,13 @@
+package com.thanh.timphongtro;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Bottom_navigation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_navigation);
+    }
+}
